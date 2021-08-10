5d86253220ce6f3e45a654ef69dba7c50b679cac
jacob kleiman <jacobakleiman@gmail.com>
2021-08-10 12:25:34 -0700

    creates Pet component

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,8 +1,16 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Lucas"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "Labrador"),
+  ]);
+};
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
