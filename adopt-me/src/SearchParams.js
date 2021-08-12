a6bc9980ef9030bed7faadd36e7faf152014358f
jacob kleiman <jacobakleiman@gmail.com>
2021-08-12 13:02:43 -0700

    creates animal and breed selector

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +19,38 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((item, key) => (
+              <option key={key} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Animal
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((item, key) => (
+              <option key={key} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
