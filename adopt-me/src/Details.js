b65bc55cb0740fff415745050efb63ce6bcdba92
jacob kleiman <jacobakleiman@gmail.com>
2021-08-12 14:48:45 -0700

    implements Details page

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,5 +1,31 @@
-const Details = () => {
-  return <h2>Hi lololololol</h2>;
+import { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Details = (props) => {
+  const [, setLoading] = useState(true);
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    fetchDetails();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function fetchDetails() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${props.match.params.id}`
+    );
+    const json = await res.json();
+    setLoading(false);
+    setDetails(json.pets[0]);
+  }
+  const { animal, breed, city, state, description, name } = details;
+  return (
+    <div className="details">
+      <h1>{name}</h1>
+      <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+      <button>Adopt ${name}</button>
+      <p>{description}</p>
+    </div>
+  );
 };
 
-export default Details;
+export default withRouter(Details);
