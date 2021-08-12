31245917995f13d8205dd9b04b6e19f53639640d
jacob kleiman <jacobakleiman@gmail.com>
2021-08-12 12:24:15 -0700

    configures eslintrc, fixes errors in App.js

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Lucas',
-      animal: 'Dog',
-      breed: 'Labrador',
-    }),
-    React.createElement(Pet, {
-      name: 'Wishy Woo',
-      animal: 'Dog',
-      breed: 'Badachadoo Urugay',
-    }),
-    React.createElement(Pet, {
-      name: 'Hocus Pocus',
-      animal: 'Dog',
-      breed: 'Labrador',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Lucas" animal="Dog" breed="Labrador" />
+      <Pet name="Wishy Woo" animal="Dog" breed="Badachadoo Urugay" />
+      <Pet name="Hocus Pocus" animal="Dog" breed="Labrador" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
