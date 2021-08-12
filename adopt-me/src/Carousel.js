2deeef7fe35a5d2efb3df392dad2c409e5ab09ac
jacob kleiman <jacobakleiman@gmail.com>
2021-08-12 15:53:53 -0700

    adds interactivity to carousel

--- adopt-me/src/Carousel.js
+++ adopt-me/src/Carousel.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const Carousel = (props) => {
-  const [active] = useState(0);
+  const [active, setActive] = useState(0);
   const [images, setImages] = useState([
     'http://pets-images.dev-apis.com/pets/none.jpg',
   ]);
@@ -10,14 +10,21 @@ const Carousel = (props) => {
     if (props.images && props.images.length) setImages(props.images);
   }, [props.images]);
 
+  const handleIndexClick = (event) => {
+    setActive(+event.target.dataset.index);
+  };
+
   return (
     <div className="carousel">
       <img src={images[active]} alt="animal" />
       <div className="carousel-smaller">
         {images.map((photo, idx) => (
+          // eslint-disable-next-line
           <img
             key={photo}
             src={photo}
+            data-index={idx}
+            onClick={handleIndexClick}
             className={idx === active ? 'active' : ''}
             alt="animal thumbnail"
           />
