a6f8c28c0f1ff4f66588e3d7dbd7d324594aae2e
jacob kleiman <jacobakleiman@gmail.com>
2021-08-13 12:56:59 -0700

    implements Modal portal

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react';
 import ThemeContext from './ThemeContext';
 import { withRouter } from 'react-router-dom';
 import Carousel from './Carousel';
+import Modal from './Modal';
 
 const Details = (props) => {
   const [loading, setLoading] = useState(true);
   const [details, setDetails] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetchDetails();
@@ -19,7 +21,13 @@ const Details = (props) => {
     setLoading(false);
     setDetails(json.pets[0]);
   }
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+  const adopt = () => (window.location = 'http://bit.ly/pet-adopt');
+
   const { animal, breed, city, state, description, name, images } = details;
+
   return (
     <div className="details">
       {loading ? (
@@ -32,10 +40,26 @@ const Details = (props) => {
             <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
             <ThemeContext.Consumer>
               {([theme]) => (
-                <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+                <button
+                  onClick={toggleModal}
+                  style={{ backgroundColor: theme }}
+                >
+                  Adopt {name}
+                </button>
               )}
             </ThemeContext.Consumer>
             <p>{description}</p>
+            {showModal ? (
+              <Modal>
+                <div>
+                  <h1>Would you like to adopt {name}?</h1>
+                  <div className="buttons">
+                    <button onClick={adopt}>Yes</button>
+                    <button onClick={toggleModal}>No</button>
+                  </div>
+                </div>
+              </Modal>
+            ) : null}
           </div>
         </>
       )}
